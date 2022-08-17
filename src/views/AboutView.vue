490810f6da52046d88bb85fe10ca91957dcff6a1
kirineko <kirineko@qq.com>
2022-08-17 10:17:06 +0800

    vue init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Panel title</h3>
+      </div>
+      <div class="panel-body">Panel content</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "about",
+      content: "This is an about page",
+    };
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <DeleteItem />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DeleteItem from "@/components/DeleteItem.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    DeleteItem,
+  },
+};
+</script>
